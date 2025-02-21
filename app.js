a55d52d15ffb3b0ea63df1c11732629ed0f67fa9
Amanda Campos <amandaellendecampos@gmail.com>
2025-02-20 23:07:49 -0300

    Adicionando array e a função de adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+
+function adicionarAmigo () {
+    let nomeAmigo = document.querySelector('input').value; {
+        console.log('O botão foi clicado');
+    };
+
+    if (nomeAmigo === '') {
+        alert('Insira um nome válido!')
+        return;
+        };
+    
+        if (amigos.includes(nomeAmigo)) {
+            alert('Este amigo já está na lista!');
+            return;
+        };
+};
