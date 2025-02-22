1b3dd6a7d641c397de79a5c3f89f87e72383afa0
Amanda Campos <amandaellendecampos@gmail.com>
2025-02-21 22:25:17 -0300

    Finalizando challenge do Amigo Secreto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos = [];
-
+let amigosDisponiveis = [];
 
 function adicionarAmigo () {
     let nomeAmigo = document.querySelector('input').value; {
@@ -13,10 +13,13 @@ function adicionarAmigo () {
         return;
         };
     
-        if (amigos.includes(nomeAmigo)) {
-            alert('Este amigo já está na lista!');
-            return;
+    if (amigos.includes(nomeAmigo)) {
+         alert('Este amigo já está na lista!');
+        return;
         };
+
+    amigos.push(nomeAmigo);
+    amigosDisponiveis = [...amigos];
 
     atualizarLista();
     document.querySelector('input').value = '';
@@ -32,4 +35,24 @@ function atualizarLista() {
         lista.appendChild(item);
         }
     );
-}
+};
+
+function sortearAmigo() {
+    if (amigos.length < 3) {
+        alert ('Insira, no minimo, três nomes para realizar o sorteio');
+        return;
+    };
+
+    if (amigosDisponiveis.length === 0) {
+        alert('Todos os amigos já foram sorteados');
+        return;
+    }
+
+    let nomeSorteado = Math.floor(Math.random() * amigosDisponiveis.length);
+    let amigoSorteado = amigosDisponiveis[nomeSorteado];
+
+    amigosDisponiveis.splice(nomeSorteado, 1);
+
+    let resultadoDaLista = document.getElementById('resultado');
+    resultadoDaLista.innerHTML = `<li>Seu amigo secreto é: ${amigoSorteado}!</li>`;
+};
